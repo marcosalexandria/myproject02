47d72d4e4adbae6aad696b450cc5347504a05581
Marcos Alexandria <marcos.alexandria01@gmail.com>
2022-07-13 10:06:45 -0300

    Update ClienteRepository.java

--- src/main/java/com/example/myproject02/domain/repositorio/ClienteRepository.java
+++ src/main/java/com/example/myproject02/domain/repositorio/ClienteRepository.java
@@ -3,7 +3,12 @@ package com.example.myproject02.domain.repositorio;
 import com.example.myproject02.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+    List<Cliente> findByNomeLike(String nome);
+
+    List<Cliente> findByNomeOrId(String nome, Integer id);
 
 }
